38449c23296443dfd894385c3423373525c5f39e
eyupalemdar <eyupalemdar@yaani.com>
2021-05-30 19:54:30 +0300

    First initialization

---
+++ Source/TestReplicatedALS/TestReplicatedALS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestReplicatedALS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestReplicatedALS, "TestReplicatedALS" );
---
+++ Source/TestReplicatedALS/TestReplicatedALSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestReplicatedALSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTREPLICATEDALS_API ATestReplicatedALSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
